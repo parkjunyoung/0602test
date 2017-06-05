74a9d74b05edb98aa89aa831376cd2f831f43a7f
JONGHO-SURFACEB\jongho <ho1234c@gmail.com>
2017-06-05 21:35:07 +0900

    temp commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,48 @@
 import express from 'express';
 import path from 'path';
- 
+
 const app = express();
- 
+
 let port = 3000;
- 
 
-app.use('/', express.static( path.join(__dirname, '../../frontend/build') ));
+var Sequelize = require('sequelize');
 
-app.get('/' , (req,res)=>{
-    res.send('index app');
+const sequelize = new Sequelize('test', 'ho1234c', 'whdghsla1!', {
+    host: 'jongho.database.windows.net',
+    dialect: 'mssql',
+    dialectOptions: {
+        encrypt: true
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
 });
 
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+app.use('/', express.static(path.join(__dirname, '../../frontend/build')));
+
+app.get('/', (req, res) => {
+    res.send('index app');
+});
 
 import posts from './routes/posts';
 app.use('/posts', posts);
 
-
-
-app.get('*', function(req,res){
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-
 // SERVE STATIC FILES - REACT PROJECT
-
 
 const server = app.listen(port, () => {
     console.log('Express listening on port', port);
