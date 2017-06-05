6f6582898a21e89b5fb553d30195d10693367a42
parkjunyoung <pjoon86@naver.com>
2017-06-05 15:38:36 +0900

    password env 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,37 @@
+import dotenv from 'dotenv';
+dotenv.config(); // LOAD CONFIG
+
 import express from 'express';
 import path from 'path';
  
 const app = express();
  
 let port = 3000;
- 
+
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize( process.env.DATABASE , process.env.DB_USER , process.env.DB_PASSWORD , {
+  host:  process.env.DB_HOST ,
+  dialect: 'mssql',
+  dialectOptions: {
+    encrypt: true,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+    .catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
 
 //app.use('/', express.static( path.join(__dirname, '../../frontend/build') ));
 
