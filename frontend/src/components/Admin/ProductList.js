118c3171d4c1e3c993801759f06e5c341e3b3b08
parkjunyoung <pjoon86@naver.com>
2017-06-08 18:33:15 +0900

    admin 등록 ui

---
+++ frontend/src/components/Admin/ProductList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div className="row">
+                <table className="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>제품명</th>
+                            <th>가격</th>
+                            <th>등록일</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <Link to="/admin/product/write" className="btn btn-primary">등록하기</Link>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
